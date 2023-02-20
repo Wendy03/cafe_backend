da02965d92397c652910132ad1a59489a2240c0a
wendy03 <wendysu03@gmail.com>
2023-02-21 02:47:06 +0800

    add article page

---
+++ src/views/frontend/ArticleView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container mt-5">
+    <ArticleCard></ArticleCard>
+  </div>
+</template>
+
+<script>
+import ArticleCard from '../../components/frontend/ArticleCard.vue';
+
+export default {
+  data() {
+    return {};
+  },
+  components: { ArticleCard },
+};
+</script>
