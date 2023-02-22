52f7eebc272cbdf84fd7a28ff175238f95691132
wendy03 <wendysu03@gmail.com>
2023-02-23 02:32:36 +0800

    fixed multi images

--- src/views/backend/AdminProducts.vue
+++ src/views/backend/AdminProducts.vue
@@ -163,10 +163,6 @@ export default {
           });
         });
     },
-    createImages() {
-      this.product.imagesUrl = [];
-      this.product.imagesUrl.push('');
-    },
     delProduct() {
       this.isProcessing = true;
       this.$http
