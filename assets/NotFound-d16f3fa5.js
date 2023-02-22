2a08546ff3a434fe423ab069a053b19a566f414e
wendy03 <wendysu03@gmail.com>
2023-02-23 02:35:56 +0800

    deploy

---
+++ assets/NotFound-d16f3fa5.js
@@ -0,0 +1,1 @@
+import{_ as t,r as o,o as s,c as a,d as n,w as r,b as c,e as _}from"./index-b7acc520.js";const d={data(){return{}}},i={class:"container m-5"},l=c("h1",{class:"mb-5"}," 404 Page Not Found ",-1);function p(u,m,f,h,x,N){const e=o("router-link");return s(),a("div",i,[l,n(e,{to:"/"},{default:r(()=>[_("回到首頁")]),_:1})])}const $=t(d,[["render",p]]);export{$ as default};
